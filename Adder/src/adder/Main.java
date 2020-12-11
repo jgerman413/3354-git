447b76fb05759eba81f43272300b40313a3eb88b
jgerman413 <71850267+jgerman413@users.noreply.github.com>
2020-12-11 04:25:39 -0600

    Update Main.java
    Changes made to add two positive integers passed as arguments.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,17 +1,26 @@
-package adder;
+File: Main.java
 
-public class Main {
-
-    public static void main(String[] args) {
-        try {
-            int result = addArguments(args);
-            System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+public class Main
+{
+        public static void main(String args[])
+        {
+                try
+                {
+                        int result = addArguments(args);
+                        System.out.println(result);
+                }
+                catch(Exception e)
+                {
+                        System.err.println("Please provide integers to add ");
+                }
         }
-    }
-
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
-    }
+        private static int addArguments(String[] args)
+        {
+                int sum = 0 ; // variable to store sum
+                for(int i=0; i < args.length; i++)// Loop to add all array element
+                {
+                        sum = sum + Integer.valueOf(args[i]); // compute sum
+                }
+                return sum; //return sum
+        }
 }
